9ddc49f95b4b1e8661d996f0509533f5e3840f6c
linh2020 <cdev.linh@gmail.com>
2024-04-27 01:45:18 -0700

    Created General Routes

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.get("/", (req, res) => {
   res.send("Task Manager API With Node JS Express and MongoDB");
 });
 
+// app.get("/api/v1/tasks"); // get all the tasks
+// app.post("/api/v1/tasks"); // create a new task
+// app.get("/api/v1/tasks/:id"); // get single task
+// app.path("/api/v1/tasks/:id"); // update task
+// app.delete("/api/v1/tasks/:id"); // delete task
+
 // server listening
 app.listen(PORT, () =>
   console.log(`Express server is listening on port ${PORT}`)
