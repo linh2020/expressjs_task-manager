f4acae7a89e5bcf6b097c4c0a0c752de69bde864
linh2020 <cdev.linh@gmail.com>
2024-05-01 11:41:41 -0700

    Basic Validation

--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema(
   {
-    name: { type: String },
-    completed: { type: Boolean },
+    name: {
+      type: String,
+      required: [true, "must provide name"],
+      trim: true,
+      maxlength: [20, "name can not be more than 20 characters"],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
