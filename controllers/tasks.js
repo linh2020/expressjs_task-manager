fe8482b420d2b5a6a7f4debe83a754710c340cca
linh2020 <cdev.linh@gmail.com>
2024-05-02 03:04:39 -0700

    Controller - Get Single Task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -11,15 +11,29 @@ const getAllTasks = async (req, res) => {
 
 const createTask = async (req, res) => {
   try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
+    const tasks = await Task.create(req.body);
+    res.status(201).json({ tasks });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-const getTask = (req, res) => {
-  res.status(200).json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    // const singleTask = await Task.findById(req.params.id);
+    // const singleTask = await Task.findOne({ _id: req.params.id });
+
+    const { id: taskID } = req.params;
+    const singleTask = await Task.findOne({ _id: taskID });
+
+    if (!singleTask) {
+      return res.status(404).json({ message: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json(singleTask);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const updateTask = (req, res) => {
