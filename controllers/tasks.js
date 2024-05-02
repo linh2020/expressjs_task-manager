9770656be4f08c6f4ccf41425abc8805c54132e7
linh2020 <cdev.linh@gmail.com>
2024-05-02 02:36:11 -0700

    Controller - Get All Tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require("../models/Task.js");
 
-const getAllTasks = (req, res) => {
-  res.status(200).send("all items from the file");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const createTask = async (req, res) => {
