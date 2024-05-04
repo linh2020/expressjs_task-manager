915fd6551fa3c942660a3432d6b4f1120952a626
linh2020 <cdev.linh@gmail.com>
2024-05-04 00:31:42 -0700

    add form-submit()

--- public/browser-app.js
+++ public/browser-app.js
@@ -50,6 +50,7 @@ const showTasks = async () => {
 showTasks();
 
 $(document).ready(function () {
+  // delete task
   $(tasksDOM).on("click", ".delete-btn", async (e) => {
     console.log(e);
     loadingDOM.style.visibility = "visible";
@@ -64,5 +65,24 @@ $(document).ready(function () {
     loadingDOM.style.visibility = "hidden";
   });
 
-  //
+  // form-submit
+  $(formDOM).submit(async function (e) {
+    e.preventDefault();
+    const name = taskInputDOM.value;
+    try {
+      await axios.post("/api/v1/tasks/", { name });
+      showTasks();
+      taskInputDOM.value = "";
+      formAlertDOM.style.display = "block";
+      formAlertDOM.textContent = `Success, task added`;
+      formAlertDOM.classList.add("text-success");
+    } catch (error) {
+      formAlertDOM.style.display = "block";
+      formAlertDOM.innerHTML = `error, please try again`;
+    }
+    setTimeout(() => {
+      formAlertDOM.style.display = "none";
+      formAlertDOM.classList.remove("text-success");
+    }, 3000);
+  });
 });
